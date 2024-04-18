2291e01e672ae857ad6645797921461b2e0cfdd0
Tony Elmore <tony@tonyelmore.com>
2024-04-18 16:40:45 -0500

    Change app name, remove config with ctor injection

--- src/main/java/com/telmore/pcftester/components/configserver/config/Configfile.java
+++ src/main/java/com/telmore/pcftester/components/configserver/config/Configfile.java
@@ -1,24 +1,34 @@
 package com.telmore.pcftester.components.configserver.config;
 
 import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.AccessLevel;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @RefreshScope
 @Component
+@Data
+@RequiredArgsConstructor
 public class Configfile {
     
-    @Value("${globalConfigVar:none}")
+    /* 
+     * This class holds all of the variables that come from the config server
+     * 
+     * The @RequiredArgsConstructor annotation is specified, but not required
+     * 
+     */
+
+    @Value("${globalConfigVar:default value for globalConfigVar}")
+    @Setter(AccessLevel.NONE)
     private String globalConfigVar;
 
     @Value("${pcftester.appSpecificVar:none}")
+    @Setter(AccessLevel.NONE)
     private String appSpecificVar;
 
-    public String getGlobalConfigVar() {
-        return globalConfigVar;
-    }
-
-    public String getAppSpecificVar() {
-        return appSpecificVar;
-    }
 }
--- src/main/java/com/telmore/pcftester/components/configserver/controllers/ConfigServerController.java
+++ src/main/java/com/telmore/pcftester/components/configserver/controllers/ConfigServerController.java
@@ -1,35 +1,34 @@
 package com.telmore.pcftester.components.configserver.controllers;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.telmore.pcftester.components.configserver.config.ConfigWithCtor;
-// import com.telmore.pcftester.components.configserver.config.Configfile;
+import com.telmore.pcftester.components.configserver.config.Configfile;
 
 @RestController
 @RequestMapping("/configserver/")
 @Conditional(ConfigServerConditional.class)
 public class ConfigServerController {
 
-    // @Autowired
-    // private Configfile configfile;
-
-    private final ConfigWithCtor configWithCtor;
-
-    public ConfigServerController(ConfigWithCtor configWithCtor) {
-        this.configWithCtor = configWithCtor;
-    }
+    @Autowired
+    private Configfile configfile;
 
     @GetMapping("test")
     public String test() {
         StringBuilder sb = new StringBuilder();
         sb.append("Config server test\n");
-        sb.append("Global config var: ").append(configWithCtor.getGlobalConfigVar()).append("\n");
-        sb.append("App specific var: ").append(configWithCtor.getAppSpecificVar()).append("\n");
+        sb.append("Global config var: ").append(configfile.getGlobalConfigVar()).append("\n");
+        sb.append("App specific var: ").append(configfile.getAppSpecificVar()).append("\n");
         return sb.toString();
     }
+
+    // Now need to do some testing with the different strategies
+    // 3.2.2 is installed
+    // So far, I've created the master branch and then added the label to the service creation
+    // Take off the label and it should be using the main branch
+
 
 }
